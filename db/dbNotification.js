120c4a37087899cb47584f7ed38b4f4122e5342f
A00079 <bajay0007@gmail.com>
2020-06-06 02:35:20 +0530

    Fix in push  notification

---
+++ db/dbNotification.js
@@ -0,0 +1,22 @@
+// import and use mongodb.MongoClient
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const dbConnectionUrl = 'mongodb+srv://Ajay007:superman@007@cluster0-sgea6.mongodb.net/DonBosco?retryWrites=true&w=majority';
+
+function Notificationinitialize(dbName, dbCollectionName, successCallback, failureCallback) {
+	MongoClient.connect(dbConnectionUrl,{ useUnifiedTopology: true }, function (err, dbInstance) {
+		if (err) {
+			console.log(`[MongoDB connection] ERROR: ${err}`);
+			failureCallback(err);        // this should be "caught" by the calling function
+		} else {
+			const dbObject = dbInstance.db(dbName);
+			const dbCollection = dbObject.collection(dbCollectionName);
+
+			console.log("[MongoDB connection] SUCCESS");
+			successCallback(dbCollection);
+		}
+	});
+}
+
+module.exports = { Notificationinitialize };
--- routes/Announcements/addAnnouncements.js
+++ routes/Announcements/addAnnouncements.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 // << db setup >>
 const db = require("../../db/db.config");
+const NotiDB = require("../../db/dbNotification");
 const dbName = "DonBosco";
 const collectionName = "Announcement";
+const collectionSubscriber = "Subscribers";
 
 db.initialize(dbName, collectionName, function (dbCollection) { // successCallback
     router.post("/", (req, response) => {
@@ -13,10 +15,21 @@ db.initialize(dbName, collectionName, function (dbCollection) { // successCallba
         dbCollection.insertOne(item, (error, result) => { // callback of insertOne
             if (error) throw error;
             // return updated list
-            dbCollection.find().toArray((_error, _result) => { // callback of find
-                if (_error) throw _error;
-                response.json(_result);
-            });
+            // dbCollection.find().toArray((_error, _result) => { // callback of find
+            //     if (_error) throw _error;
+            //     response.json(_result);
+            // });
+            NotiDB.Notificationinitialize(dbName, collectionSubscriber, function (secdbCollection){
+                secdbCollection.find().toArray((_error, _result) => { // callback of find
+                    if (_error) throw _error;
+                    let tokenArry = [];
+                    _result.map((item) =>{
+                        tokenArry.push(item.token);
+                    })
+                    response.json(tokenArry);
+
+                });
+            })
         });
     });
 }, function (err) { // failureCallback
